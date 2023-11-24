89eabc3ac86f037afd0473e880558bad25134da2
Quienten <quientenmiller2@gmail.com>
2023-11-24 12:32:45 -0800

    Setup client server

---
+++ server/test_openai.js
@@ -0,0 +1,34 @@
+import OpenAI from "openai";
+
+import readline from "readline"
+
+const openai = new OpenAI({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+const userInterface = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+userInterface.prompt()
+userInterface.on("line", async input => {
+    const res = await openai.chat.completions.create({
+        messages: [{ role: "user", content: input }],
+        model: "gpt-3.5-turbo",
+    });
+
+    console.log(res.choices);
+    userInterface.prompt()
+})
+
+// async function main() {
+//     const res = await openai.chat.completions.create({
+//         messages: [{ role: "user", content: "How are you ChatGPT?" }],
+//         model: "gpt-3.5-turbo",
+//     });
+//
+//     console.log(res.choices);
+// }
+
+//main();
